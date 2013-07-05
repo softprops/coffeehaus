9e745ebdf5dba45ec56c1500d29cd633fb9038d7
softprops <d.tangren@gmail.com>
2013-07-05 14:36:14 -0400

    update tests

--- src/test/scala/VanillaCompileSpec.scala
+++ src/test/scala/VanillaCompileSpec.scala
@@ -3,11 +3,20 @@ package coffeehaus
 class VanillaCompileSpec extends CoffeehausSpec {
   describe("Vanilla Compile") {
     it ("should compile vanilla coffee") {
-      assert(Compile.vanilla("alert 'hello'") === Right(file("/vanilla/basic.js")))
+      assert(Compile.vanilla("alert 'hello'") ===
+        Right(JsSource(file("/vanilla/basic.js"))))
     }
     
     it ("should compile bare coffee") {
-      assert(Compile.vanilla("alert 'hello'", Options(bare = true)) == Right(file("/vanilla/bare.js")))
+      assert(Compile.vanilla("alert 'hello'", Options(bare = true)) ===
+        Right(JsSource(file("/vanilla/bare.js"))))
+    }
+
+    it ("should compile source maps") {
+      val coffeeFile = "/Users/dougtangren/code/scala/projects/coffeehaus/src/test/resources/vanilla/sourcemap.coffee"
+      assert(Compile.vanilla(file("/vanilla/sourcemap.coffee"),
+        Options(sourceMap = true, inline = Some(true))) ===
+        Right(SourceMapping(file("/vanilla/sourcemap.js"), file("/vanilla/sourcemap.map"))))
     }
   }
 }
