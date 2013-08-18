8e837a4a10f66a4e4b974041e4644a8fa091067b
softprops <d.tangren@gmail.com>
2013-08-18 01:18:23 -0400

    tests compile

--- src/test/scala/IcedCompileSpec.scala
+++ src/test/scala/IcedCompileSpec.scala
@@ -3,12 +3,12 @@ package coffeehaus
 class IcedCompileSpec extends CoffeehausSpec {
   describe("Iced Compile") {
     it ("should compile iced coffee") {
-      assert(Compile.iced("alert 'hello'") ===
+      assert(Compile.iced()("alert 'hello'") ===
         Right(JsSource(file("/iced/basic.js"))))
     }
 
     it ("should compile bare coffee") {
-      assert(Compile.iced("alert 'hello'", Options(bare = true)) ===
+      assert(Compile.iced(Options(bare = true))("alert 'hello'") ===
         Right(JsSource(file("/iced/bare.js"))))
     }
   }
--- src/test/scala/VanillaCompileSpec.scala
+++ src/test/scala/VanillaCompileSpec.scala
@@ -3,19 +3,18 @@ package coffeehaus
 class VanillaCompileSpec extends CoffeehausSpec {
   describe("Vanilla Compile") {
     it ("should compile vanilla coffee") {
-      assert(Compile.vanilla("alert 'hello'") ===
+      assert(Compile.vanilla()("alert 'hello'") ===
         Right(JsSource(file("/vanilla/basic.js"))))
     }
     
     it ("should compile bare coffee") {
-      assert(Compile.vanilla("alert 'hello'", Options(bare = true)) ===
+      assert(Compile.vanilla(Options(bare = true))("alert 'hello'") ===
         Right(JsSource(file("/vanilla/bare.js"))))
     }
 
     it ("should compile source maps") {
       val coffeeFile = "/Users/dougtangren/code/scala/projects/coffeehaus/src/test/resources/vanilla/sourcemap.coffee"
-      assert(Compile.vanilla(file("/vanilla/sourcemap.coffee"),
-        Options(sourceMap = true, inline = Some(true))) ===
+      assert(Compile.vanilla(Options(sourceMap = true, inline = Some(true)))(file("/vanilla/sourcemap.coffee")) ===
         Right(SourceMapping(file("/vanilla/sourcemap.js"), file("/vanilla/sourcemap.map"))))
     }
   }
